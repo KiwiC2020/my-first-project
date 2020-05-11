c92729011e16970cc277321d0e97b1de38e25328
KiwiC2020 <aadithegamer@gmail.com>
2020-05-11 01:53:39 +0000

    Wrote some basic python code

---
+++ main.py
@@ -0,0 +1,36 @@
+# this is some insane math coding
+print ('45 * 45 =' + str(45 * 45) )
+print ('265 * 5 =' + str(265 * 5) )
+print()
+
+
+9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+9
